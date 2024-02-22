62b18cdbdef5b4f6754ffe74c83780774ef6f47b
njabulodark <njabuloadvocate2001@gmail.com>
2024-02-22 15:45:39 +0200

    fix driver support

--- app/src/main/java/application/Flight.java
+++ app/src/main/java/application/Flight.java
@@ -47,6 +47,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Flight {
     private WebDriver driver;
     private JSONObject data;
@@ -61,7 +63,11 @@ public class Flight {
 
     public Flight(){
         // Create a new instance of the ChromeDriver
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\njabulo\\Documents\\code\\application\\app\\src\\main\\java\\application\\chromedriver.exe");
+        // driver location is current directory
+        
+        
+        // System.setProperty("webdriver.chrome.driver", "C:\\Users\\njabulo\\Documents\\code\\application\\app\\src\\main\\java\\application\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
 
         // Configure Chrome options
         ChromeOptions options = new ChromeOptions();
